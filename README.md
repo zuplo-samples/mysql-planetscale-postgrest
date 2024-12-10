bdb6c08b19c999e856e6e1c4a34ecdf4c7a75b3f
Adrian Machado <amachado@uwaterloo.ca>
2024-12-10 00:23:17 -0800

    Aiven demo #1
    * Switch to Aiven MySQL
    
    * Remove planetscale refs
    
    * err handlings
    
    * Proper mysql impl

--- README.md
+++ README.md
@@ -1,19 +1,77 @@
-## PlanetREST
+# MySQL PostgREST
 
-Demo application to build a REST API over a planetscale database.
+Do you wish you could interact with your MySQL database via API/SDK like Supabase? Look no further! This project shows you exactly how to recreate the Supabase API and developer experience.
 
-1. Create a Planetscale account and set up a DB connection
-2. Create a table following [their quickstart](https://planetscale.com/docs/tutorials/planetscale-quick-start-guide)
-3. Set env vars from step 1
+## Installation
 
+This is just a template so you can clone this repository directly and run it yourself.
+
+1. Create a hosted MySQL database (ex. using [Aiven's free tier](https://aiven.io/pricing?product=mysql)) and grab the connection parameters.
+
+![Aiven connection params](image.png)
+
+2. Create a table following [planetscale's quickstart](https://planetscale.com/docs/tutorials/planetscale-quick-start-guide)
+
+```sql
+CREATE TABLE products (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  image_url varchar(255),
+  category_id INT,
+  KEY category_id_idx (category_id)
+);
+```
+
+3. Create an `admin` role and grant it full access
+
+```sql
+CREATE ROLE 'admin'
+GRANT ALL on app_db.* to 'admin'
+```
+
+4. Grant your user the admin role. For example:
+
+```sql
+GRANT 'admin' to 'avnadmin';
+```
+
+Alternatively you can use the included `permissions.json` file to determine access and roles.
+
+5. Create a `.env` file at the root and set env vars from step 1
+
+```
 NEXT_PUBLIC_API_URL=# ex. "http://localhost:3000"
 DATABASE_HOST=# ex. "aws.connect.psdb.cloud"
 DATABASE_USERNAME=
 DATABASE_PASSWORD=
+DATABASE_PORT=
+DATABASE_CA_CERTIFICATE=
+```
 
-4. Install and run
+6. Install and run
 
 ```bash
 npm install
 npm run dev
 ```
+
+## How It Works
+
+![img](./public/diagram.png)
+
+PostgREST requests are sent from the front-end to a catch-all route which invokes a serverless function. That function translates the request into a SQL query and executes it against your Neon database. The results are returned as a JSON.
+
+NOTE: I wouldn't recommend actually performing SQL queries from your front-end - I bundled this together for demonstration purposes. You are essentially providing a door for malicious actors to interact with your database from your front-end. Instead, I would host the PostgREST API in a separate project (and protected behind a gateway), and call it from CRUD RESTful serverless functions on this project. Alternatively, I think you can implement stuff like row-level security and anon keys to make this method more secure, which is what Supabase does.
+
+## Tools
+
+I used the following tools:
+
+1. NextJS Serverless Functions for easy Typescript setup + fast runtime (Cloudflare Workers will also work, but you won't be able to read files using node libs).
+2. Vercel to simplify deployment (you can use Cloudflare pages too).
+3. [`@supabase/postgrest-js`](https://www.npmjs.com/package/@supabase/postgrest-js) to construct queries using a Supabase-like SDK. This sends PostgREST requests to your serverless function.
+4. [`@subzerocloud/rest`](https://www.npmjs.com/package/@subzerocloud/rest) to translate the PostgREST requests into Postgres queries.
+5. [`mariadb`](https://www.npmjs.com/package/mariadb) for querying your MySQL database.
+6. (optional) [Zuplo API Gateway](https://zuplo.com) to proxy your serverless function. Good place to add caching, rate limiting, DDOS/bot protection, etc so this doesn't blow up in production. You can find the gateway code [here](https://github.com/zuplo-samples/neon-postgrest-proxy).
+
+Created with ❤️ by [Zuplo](https://zuplo.com).
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,34 +2,26 @@
 
 import { PostgrestClient } from "@supabase/postgrest-js";
 import { useState } from "react";
-// import Image from "next/image";
+import Image from "next/image";
 
 // Replace the NEXT_PUBLIC_API_URL with your Zuplo gateway URL (or your Vercel URL directly
 // if you don't want built-in protection or caching)
 const REST_URL = `${process.env.NEXT_PUBLIC_API_URL}/api`;
 
-interface PostgrestResponse {
-  body: string;
-  constraints_satisfied: boolean;
-  page_total: number;
-  response_headers: Headers | null;
-  response_status: number | null;
-  total_result_set: number;
-}
-
 const SELECT_CODE_SAMPLE = `const { data, error } = await postgrest
-      .from("playing_with_neon")
+      .from("products")
       .select("*")
       .order("id", { ascending: false });`;
 const getInsertCodeSample = (
-  name: string,
-  value: number
-) => `const { data, error } = await postgrest
-      .from("playing_with_neon")
-      .insert({ name: ${name}, value: ${value} });`;
+  name: string
+) => `const { data, error } = await postgrest.from("products").insert({
+      name: ${name},
+      image_url: "https://example.com/500x500",
+      category_id: 1,
+    });`;
 
 export default function Home() {
-  const [neonData, setNeonData] = useState<string>();
+  const [mysqlData, setMysqlData] = useState<unknown>();
   const [error, setError] = useState<string>();
   const [codeSample, setCodeSample] = useState<string>();
   const handleFetchClick = async () => {
@@ -43,46 +35,47 @@ export default function Home() {
       .select("*")
       .order("id", { ascending: false });
     if (data) {
-      setNeonData(JSON.parse((data as unknown as PostgrestResponse[])[0].body));
+      setMysqlData(data);
       setError(undefined);
     }
     if (error) {
       setError(JSON.stringify(error, null, 2));
-      setNeonData(undefined);
+      setMysqlData(undefined);
     }
   };
   const handleInsertClick = async () => {
     const randomName = Math.random().toString(36).substring(7);
-    const random = Math.random();
     const postgrest = new PostgrestClient(REST_URL, {
       headers: {
-        Prefer: "return=minimal",
+        Prefer: "return=representation",
       },
     });
     const { data, error } = await postgrest.from("products").insert({
       name: randomName,
-      imageUrl: "https://example.com/500x500",
+      image_url: "https://example.com/500x500",
       category_id: 1,
     });
     if (data) {
-      performFetch();
+      await performFetch();
     }
     if (error) {
       setError(JSON.stringify(error, null, 2));
+      setMysqlData(undefined);
     }
-    setCodeSample(getInsertCodeSample(randomName, random));
+    setCodeSample(getInsertCodeSample(randomName));
   };
   return (
     <div className="min-h-screen font-[family-name:var(--font-geist-sans)] flex p-8 items-center flex-col gap-y-4">
-      <header className="text-4xl font-medium text-green-400">
-        Neon PostgREST API Demo
+      <header className="text-4xl font-medium text-[#02758f]">
+        <span className="text-[#02758f]">My</span>
+        <span className="text-[#f29111]">SQL</span> PostgREST API Demo
       </header>
       <div>
-        A Supabase-like API DevX with Neon and PostgREST.{" "}
+        A Supabase-like API DevX with Mysql and PostgREST.{" "}
         <a
-          className="text-green-400 hover:text-green-500"
+          className="text-blue-500 hover:text-blue-600"
           target="_blank"
-          href="https://github.com/zuplo-samples/neon-postgrest"
+          href="https://github.com/zuplo-samples/mysql-postgrest"
         >
           Source
         </a>
@@ -91,13 +84,13 @@ export default function Home() {
       <div className="flex gap-x-4">
         <button
           onClick={handleFetchClick}
-          className="bg-white text-black rounded-lg w-fit font-mono p-4 hover:bg-green-600 hover:text-white"
+          className="bg-white text-black rounded-lg w-fit font-mono p-4 hover:bg-[#02758f] hover:text-white"
         >
           Fetch Table Data
         </button>
         <button
           onClick={handleInsertClick}
-          className="bg-white text-black rounded-lg w-fit font-mono p-4 hover:bg-green-600 hover:text-white"
+          className="bg-white text-black rounded-lg w-fit font-mono p-4 hover:bg-[#f29111] hover:text-white"
         >
           Add Random Data
         </button>
@@ -105,18 +98,18 @@ export default function Home() {
       {codeSample ? (
         <div className="w-full">
           <h2 className="text-2xl font-medium ">Code Sample</h2>
-          <div className="bg-slate-800 text-green-400 font-mono p-4 rounded-lg shadow-md w-full max-h-[50vh] overflow-auto">
+          <div className="bg-slate-800 text-white font-mono p-4 rounded-lg shadow-md w-full max-h-[50vh] overflow-auto">
             <pre className="overflow-auto">{codeSample}</pre>
           </div>
         </div>
       ) : null}
-      {neonData ? (
+      {mysqlData ? (
         <div className="w-full">
           <h2 className="text-2xl font-medium ">Data</h2>
 
-          <div className="bg-slate-800 text-green-400 font-mono p-4 rounded-lg shadow-md w-full max-h-[50vh] overflow-auto">
+          <div className="bg-slate-800 text-white font-mono p-4 rounded-lg shadow-md w-full max-h-[50vh] overflow-auto">
             <pre className="whitespace-pre-wrap break-words">
-              {JSON.stringify(neonData, null, 2)}
+              {JSON.stringify(mysqlData, null, 2)}
             </pre>
           </div>
         </div>
@@ -126,21 +119,16 @@ export default function Home() {
           <pre className="whitespace-pre-wrap break-words">{error}</pre>
         </div>
       )}
-      {/* <footer className="w-full justify-self-end mt-8">
+      <footer className="w-full justify-self-end mt-8">
         <div className="flex justify-center text-xl pb-3">BUILT WITH</div>
         <div className="flex gap-x-8 items-center w-full justify-center flex-wrap">
           <Image src="/vercel.svg" alt="Vercel Logo" width={32} height={32} />
-          <Image
-            src="/neon-logo-dark-mono.svg"
-            alt="Neon Logo"
-            width={78}
-            height={32}
-          />
+          <Image src="/aiven.png" alt="Aiven Logo" width={40} height={40} />
           <Image src="/zuplo.svg" alt="Zuplo Logo" width={78} height={32} />
           <Image src="/next.svg" alt="NextJS Logo" width={78} height={32} />
           <Image src="/subzero.png" alt="Subzero Logo" width={40} height={40} />
         </div>
-      </footer> */}
+      </footer>
     </div>
   );
 }
