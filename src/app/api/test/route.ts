f04aa828ab6b196fa3aac69513fc6c7a300eb37c
Adrian Machado <amachado@uwaterloo.ca>
2024-12-20 16:56:50 -0800

    Test timeout

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,30 @@
+export async function OPTIONS() {
+  return new Response(null, {
+    status: 204,
+    headers: {
+      "access-control-allow-origin": "*",
+      "access-control-allow-methods": "GET, POST, PUT, DELETE, PATCH",
+      "access-control-allow-headers": "Content-Type, Prefer",
+    },
+  });
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function example() {
+  console.log("Start");
+
+  // Sleep for 3 seconds
+  await sleep(3000);
+
+  console.log("End");
+}
+
+example();
+
+export const GET = async () => {
+  await sleep(32000);
+  return new Response("Hello World!");
+};
